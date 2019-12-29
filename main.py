b246b0dd75aea2fd475c505c1193dac5020cb8c1
Yuan <yuan.yao@ing.com>
2019-12-29 17:41:32 +0100

    feat: add file selection in index window

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-import test
+import tkinter as t
+
+from tkinter import filedialog
+
+window = t.Tk()
+window.title("Kassenbericht")
+window.geometry("500x300")
+event = t.StringVar()
+on_hit = False
 
 
-print('创建新的表格按1， 已有表格按2')
-start = input()
-print('输入表格名称')
-table_name = input()
-if start == '1':
-    atable = test.Table()
-    atable.create()
-elif start == '2':
-    pass
+def click_button():
+    get_files()
 
+
+def get_files():
+    window.filename = filedialog.askopenfilename(
+        initialdir="/",
+        title="Select file",
+        filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")),
+    )
+    print(window.filename)
+
+
+select_file_button = t.Button(
+    window,
+    text="choose the directory",
+    font=("Arial", 12),
+    width=10,
+    height=1,
+    command=click_button,
+)
+select_file_button.pack()
+window.mainloop()
